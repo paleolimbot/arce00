da071f20e9f2d6532ccb041c7fd8e868a7eaa94f
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2020-10-28 11:03:21 -0300

    more plausible check results

--- tests/testthat/test-e00-convert.R
+++ tests/testthat/test-e00-convert.R
@@ -4,6 +4,6 @@ test_that("e00_convert() works", {
   cov <- e00_temp_coverage()
   coverage <- e00_convert(e00, cov)
   expect_true(dir.exists(coverage))
-  expect_is(sf::read_sf(coverage, layer = "LAB"), "sf")
+  expect_is(sf::read_sf(coverage, layer = "LAB", as_tibble = FALSE), "sf")
   unlink(coverage, recursive = TRUE)
 })
